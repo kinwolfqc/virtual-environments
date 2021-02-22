7c4ee565a4552a8484b980c44f6af635313b9aec
Sylvain <kinwolf@gmail.com>
2021-02-22 15:54:01 -0500

    Update GenerateResourcesAndImage.ps1

--- helpers/GenerateResourcesAndImage.ps1
+++ helpers/GenerateResourcesAndImage.ps1
@@ -204,6 +204,5 @@ Function GenerateResourcesAndImage {
         -var "resource_group=$($ResourceGroupName)" `
         -var "storage_account=$($storageAccountName)" `
         -var "install_password=$($InstallPassword)" `
-        -var "github_feed_token=$($GithubFeedToken)" `
         $builderScriptPath
 }
